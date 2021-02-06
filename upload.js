983ff213183e94e8f42aef4813092d8725a7babc
Advisorrry <sypikeh@yandex.com>
2021-02-06 13:24:32 +0500

    реализовано превью фото, название и размер

--- upload.js
+++ upload.js
@@ -1,7 +1,18 @@
-export function upload(selector, options = {}) {
-    const input = document.querySelector(selector)
+function bytesToSize(bytes) {
+    const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB']
+    if (!bytes) return '0 Byte'
+    const i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)))
+    return Math.round(bytes / Math.pow(1024, i)) + ' ' + sizes[i]
+}
 
+export function upload(selector, options = {}) {
+    let files = []
+
+    const input = document.querySelector(selector)
+    const preview = document.createElement('div')
     const open = document.createElement('button')
+
+    preview.classList.add('preview')
     open.classList.add('card__btn')
     open.textContent = 'открыть'
 
@@ -13,19 +24,60 @@ export function upload(selector, options = {}) {
         input.setAttribute('accept', options.accept.join(','))
     }
 
+    input.insertAdjacentElement('afterend', preview)
     input.insertAdjacentElement('afterend', open)
-
 
     const triggerInput = () => input.click()
     const changeHandler = (event) => {
-        if(!event.target.files) {
+        if (!event.target.files) {
             return
         }
 
-        const {files} = event.target
-    } 
+        files = Array.from(event.target.files)
 
+        preview.innerHTML = ''
+        files.forEach((file) => {
+            if (!file.type.match('image')) {
+                return
+            }
+
+            const reader = new FileReader()
+
+            reader.onload = (ev) => {
+                const src = ev.target.result
+                preview.insertAdjacentHTML(
+                    'afterbegin',
+                    `
+                    <div class="preview__image">
+                    <div class="preview__remove" data-name="${file.name}">&times;</div>
+                        <img src="${src}" alt="${file.name}" >
+                        <div class="preview__info">
+                            <span>${file.name}</span>
+                            ${bytesToSize(file.size)}
+                        </div>
+                    </div>
+                `,
+                )
+            }
+
+            reader.readAsDataURL(file)
+        })
+    }
+
+    const removeHandler = (event) => {
+        if (!event.target.dataset.name) {
+            return
+        }
+        const { name } = event.target.dataset
+        files = files.filter((file) => {
+            file.name !== name
+        })
+
+        const block = preview.querySelector(`[data-name="${name}"]`)
+        .closest('.preview__image')
+    }
 
     open.addEventListener('click', triggerInput)
     input.addEventListener('change', changeHandler)
+    preview.addEventListener('click', removeHandler)
 }
